ac6825807359ba3eed31e34808d0a61cd936e6b7
guanfengliu <bzrobotdoc@gmail.com>
2021-12-27 15:36:28 -0800

    remove useless code folder

--- oldversion-M-PRM/TriangPolygon.m
+++
@@ -1,6 +0,0 @@
-function fv = TriangPolygon (coord_input)
-%% Make a simple polygon and then triangulate it
-pgon=polyshape(coord_input'); 
-T=triangulation(pgon);
-fv.vertices = T.Points;
-fv.faces = T.ConnectivityList; 
